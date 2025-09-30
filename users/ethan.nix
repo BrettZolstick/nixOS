f6065532ef7b47bccef39691b6de95d18fe77dec
Ethan <crazyeman83@gmail.com>
2025-09-30 04:22:18 -0400

    modularizing the rest of home.nix

--- users/ethan.nix
+++ users/ethan.nix
@@ -1,7 +1,7 @@
 {config, pkgs, home-manager, lib, ... }: {
 
 			# import a list of all .nix files recursively under this directory		   		  	v-------------V
-	imports = lib.filter (n: lib.strings.hasSuffix ".nix" n) (lib.filesystem.listFilesRecursive ../homeManager);
+	#imports = lib.filter (n: lib.strings.hasSuffix ".nix" n) (lib.filesystem.listFilesRecursive ../homeManager);
 
 	users.users.ethan = {
 		isNormalUser = true;
