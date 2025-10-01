bbee3471d2dc6deffa9608a034cfa136472018c1
Ethan <crazyeman83@gmail.com>
2025-09-30 20:10:45 -0400

    added osu-lazer

---
+++ homeManager/additionalApps/osu-lazer.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		osu-lazer.enable = lib.mkOption {
+			default = true;	
+		};
+	};
+	
+	config = lib.mkIf config.osu-lazer.enable {
+		# Actual content of the module goes here:
+		home.packages = with pkgs; [ osu-lazer ];		
+	};	
+}
