61fbcd650c610bcde4aead87e388449dc05d86b5
Ethan <crazyeman83@gmail.com>
2025-10-01 01:33:07 -0400

    added pipewire support

--- nixOSConfiguration/baseSystemPackages/services/pipewire.nix
+++ nixOSConfiguration/baseSystemPackages/services/pipewire.nix
@@ -9,8 +9,15 @@
 
 	config = lib.mkIf config.pipewire.enable {
 		# Actual content of the module goes here:
-		
-		services.pipewire.enable = true;
+
+		security.rtkit.enable = true;
+		services.pipewire = {
+			enable = true;
+			alsa.enable = true;
+			alsa.support32Bit = true;
+			pulse.enable = true;
+			jack.enable = true;	
+		};
 
 	};
 	
