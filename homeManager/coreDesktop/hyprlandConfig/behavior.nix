420ca61744b0001fd30abccc862bebb71506aa51
Ethan <crazyeman83@gmail.com>
2025-10-02 06:45:10 -0400

    configuring hyprland

--- homeManager/coreDesktop/hyprlandConfig/behavior.nix
+++ homeManager/coreDesktop/hyprlandConfig/behavior.nix
@@ -16,9 +16,8 @@
 				border_overlap	= false;	# if true, windows snap such that only one border’s worth of space is between them
 				respect_gaps	= false;	# if true, snapping will respect gaps between windows(set in general:gaps_in)
 			};
-
-			ecosystem.no_update_news = true; # disable hyprland support message
-			
 		};	
+
+		ecosystem.no_update_news = true; # disable hyprland support message
 	};
 }
