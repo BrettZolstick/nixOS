644ce18ca45ee0a3dc16d5c68fc65717197adbdb
Ethan <crazyeman83@gmail.com>
2025-10-02 07:52:23 -0400

    adding nordzy cursors to gaucho theme

--- themes/gaucho.nix
+++ themes/gaucho.nix
@@ -1,9 +1,16 @@
 { config, lib, stylix, pkgs, ...}: {
 
+	# Colors
 	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/brewer.yaml";
 
+	# Wallpaper
 	stylix.image = ./wallpapers/culprate-gaucho.jpg;
 
+	# Cursors
+	stylix.cursor.package = pkgs.nordzy-cursor-theme;
+	#stylix.cursor.name = "";
+
+	# Fonts	
 	stylix.fonts = {
 		serif = {
 			package = pkgs.dejavu_fonts;
