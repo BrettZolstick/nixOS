cb7799ac4725138014d99bc66f26bcaffec759db
Ethan <crazyeman83@gmail.com>
2025-10-03 04:12:51 -0400

    Added wallpapers

---
+++ themes/crepuscularRays.nix
@@ -0,0 +1,33 @@
+{ config, lib, stylix, pkgs, ...}: {
+
+	# Colors
+	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/brewer.yaml";
+
+	# Wallpaper
+	stylix.image = ./wallpapers/culprate-gaucho.jpg;
+
+	# Cursors
+	#stylix.cursor.package = pkgs.nordzy-cursor-theme;
+	#stylix.cursor.name = "Nordzy-cursors";
+			  	
+	# Fonts	
+	stylix.fonts = {
+		serif = {
+			package = pkgs.dejavu_fonts;
+			name = "DejaVu Serif";						
+		};
+		sansSerif = {
+			package = pkgs.dejavu_fonts;
+			name = "DejaVu Sans";
+		};
+		monospace = {
+			package = pkgs.nerd-fonts.jetbrains-mono;
+			name = "Jetbrains Mono Nerd Font";
+		};
+		emoji = {
+			package = pkgs.noto-fonts-emoji;
+			name = "Noto Color Emoji";
+		};
+	};
+
+}
