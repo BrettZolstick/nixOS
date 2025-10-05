847e539be14b1fed493f5840a47ef6a823ec824b
BrettZolstick <64443654+BrettZolstick@users.noreply.github.com>
2025-10-05 00:36:02 -0400

    changed gitBehind interval to 5 seconds for testing

--- homeManager/coreDesktop/waybar.nix
+++ homeManager/coreDesktop/waybar.nix
@@ -81,7 +81,7 @@ in
 				};
 				"custom/gitBehind" = {
 					exec = "${gitBehind}/bin/git-behind";
-					interval = 300;
+					interval = 5;
 					return-type = "json";
 					format = "{text}";
 					on-click = "kitty --directory ~/nixOS"; 
