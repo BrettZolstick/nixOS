f13948cadb04dd9c7f9f63a9ada5e32bb99c6870
Ethan <crazyeman83@gmail.com>
2025-10-06 17:31:30 -0400

    made rebuild bind reference the current host dynamically instead of just building ethanDesktop

--- homeManager/coreDesktop/hyprlandConfig/keybinds.nix
+++ homeManager/coreDesktop/hyprlandConfig/keybinds.nix
@@ -22,7 +22,7 @@
 			"$mainMod,			B, 		exec,	$browser"
 			"$mainMod,			E,		exec,	$fileManager"
 			"$mainMod SHIFT,	S,		exec,	grimblast --freeze copy area"
-			"$mainMod,			R, 		exec, 	$terminal /run/current-system/sw/bin/bash -lc 'sudo nixos-rebuild switch --flake $HOME/nixOS##ethanDesktop --impure --show-trace; ec=$?; if [ $ec -eq 0 ]; then exit 0; else echo; echo \"Rebuild failed (exit $ec). Press Enter to close…\"; read -r _; exit $ec; fi'"
+			"$mainMod,			R, 		exec, 	$terminal /run/current-system/sw/bin/bash -lc 'sudo nixos-rebuild switch --flake $HOME/nixOS##$hostname --impure --show-trace; ec=$?; if [ $ec -eq 0 ]; then exit 0; else echo; echo \"Rebuild failed (exit $ec). Press Enter to close…\"; read -r _; exit $ec; fi'"
 			
 			# window actions
 			"$mainMod,	Q,	killactive"		
