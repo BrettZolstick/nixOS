704e2cab87e40cc191c374511fe565afc3788011
Ethan <crazyeman83@gmail.com>
2025-10-06 06:42:38 -0400

    added steam (fixes #6)

---
+++ nixOSConfiguration/baseSystemPackages/programs/steam.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+
+	options = {
+		steam.enable = lib.mkOption {
+			default = true;
+		};
+	};
+
+	config = lib.mkIf config.steam.enable {
+		# Actual content of the module goes here:
+		
+		programs.steam.enable = true;
+
+	};
+	
+}
