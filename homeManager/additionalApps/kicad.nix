6d5e14fc3ad891542339abf3bb1fe4e42d0b2d2c
Ethan <crazyeman83@gmail.com>
2025-10-07 12:54:47 -0400

    added kicad

---
+++ homeManager/additionalApps/kicad.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		kicad.enable = lib.mkOption {
+			default = true;	
+		};
+	};
+	
+	config = lib.mkIf config.kicad.enable {
+		# Actual content of the module goes here:
+		home.packages = with pkgs; [ kicad ];		
+	};	
+}
