7a80956a7e34204d467646de292b02fc12a89708
Ethan <crazyeman83@gmail.com>
2025-10-09 20:21:54 -0400

    added bambu studio because orca slicer is having weird network issues and I cant be bothered to fix rn lol

---
+++ homeManager/additionalApps/bambuStudio.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		bambuStudio.enable = lib.mkOption {
+			default = true;	
+		};
+	};
+	
+	config = lib.mkIf config.bambuStudio.enable {
+		# Actual content of the module goes here:
+		home.packages = with pkgs; [ bambu-studio ];		
+	};	
+}
