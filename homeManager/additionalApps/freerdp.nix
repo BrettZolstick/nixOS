59eed8d55a2535721a5405edad3bc442362e1a4a
Ethan <crazyeman83@gmail.com>
2025-10-11 14:35:16 -0400

    making desktop entry for rdp into my server

--- homeManager/additionalApps/freerdp.nix
+++ homeManager/additionalApps/freerdp.nix
@@ -1,4 +1,20 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: 
+let
+	rdpScript = pkgs.writeShellScriptBin "rdp-ethan-server" ''
+		#!/usr/bin/env bash
+
+		# make this file, leave the server password in it, and chmod 600 it
+		PW="$(cat $HOME/nixOS/homeManager/additionalApps/freerdp-servertron9000.pass)"
+
+		exec ${pkgs.freerdp}/bin/xfreerdp \
+		/v:192.168.68.63 \
+		/u:'servertron9000' \
+		/p:"$PW" \
+		/cert:ignore \
+		/f /rfx /clipboard
+	'';
+in
+{
 
 	# This is wrapped in an option so that it can be easily toggled elsewhere.
 	options = {
@@ -9,11 +25,11 @@
 	
 	config = lib.mkIf config.freerdp.enable {
 		# Actual content of the module goes here:
-		home.packages = with pkgs; [ freerdp ];	
+		home.packages = with pkgs; [ freerdp rdpScript ];	
 
 		xdg.desktopEntries."Ethan-Server" = {
 			name = "Ethan Server (RDP)";
-			exec = "${pkgs.freerdp}/bin/xfreerdp /v:192.168.68.63 /u:servertron9000 /p:./freerdp-servetron9000.pass /cert:ignore /f /rfx /clipboard";
+			exec = "rdp-ethan-server";
 			terminal = false;
 			icon = "computer";
 		};
