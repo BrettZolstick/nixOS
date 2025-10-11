89a9e9b789d6558aadfa1094e5e0b861e9a7f5f4
Ethan <crazyeman83@gmail.com>
2025-10-11 14:21:04 -0400

    making desktop entry for rdp into my server

--- homeManager/additionalApps/freerdp.nix
+++ homeManager/additionalApps/freerdp.nix
@@ -9,6 +9,14 @@
 	
 	config = lib.mkIf config.freerdp.enable {
 		# Actual content of the module goes here:
-		home.packages = with pkgs; [ freerdp ];		
+		home.packages = with pkgs; [ freerdp ];	
+
+		xdg.desktopEntries."Ethan-Server" = {
+			name = "Ethan Server (RDP)";
+			exec = "${pkgs.freerdp}/bin/xfreerdp /v:192.168.68.63 /u:servertron9000 /p:./freerdp-servetron9000.pass /cert:ignore /f /rfx /clipboard";
+			terminal = false;
+			icon = "computer";
+		};
+		
 	};	
 }
