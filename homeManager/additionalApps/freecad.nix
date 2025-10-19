1733a5dadf04daece2e11d242391a9a3d91a1691
Ethan <crazyeman83@gmail.com>
2025-10-18 22:04:06 -0400

    forced x11 backend on freecad to fix freezing (fixes #28)

--- homeManager/additionalApps/freecad.nix
+++ homeManager/additionalApps/freecad.nix
@@ -9,6 +9,36 @@
 	
 	config = lib.mkIf config.freecad.enable {
 		# Actual content of the module goes here:
-		home.packages = with pkgs; [ freecad-wayland ];		
+		home.packages = with pkgs; [ freecad ];	
+
+		xdg.desktopEntries."org.freecad.FreeCAD" = {
+					name = "FreeCAD";
+					exec = "env QT_QPA_PLATFORM=xcb FreeCAD - --single-instance %F";
+					icon = "org.freecad.FreeCAD";
+					comment = "Feature based Parametric Modeler";
+					terminal = false;
+					mimeType = [
+						"application/x-extension-fcstd"
+						"model/obj"
+						"image/vnd.dwg"
+						"image/vnd.dxf"
+						"model/vnd.collada+xml"
+						"application/iges"
+						"model/iges"
+						"model/step"
+						"model/step+zip"
+						"model/stl"
+						"application/vnd.shp"
+						"model/vrml"
+					];
+					categories = [ 
+						"Graphics"
+						"Science"
+						"Education"
+						"Engineering"
+						"X-CNC"
+					 ];
+					startupNotify = true;
+				};	
 	};	
 }
