910368e86fed24cf9375cb900e64bb0d52621634
Ethan <crazyeman83@gmail.com>
2025-10-22 06:57:33 -0400

    added eclipse terumin and zulu java 24

---
+++ nixOSConfiguration/baseSystemPackages/javaVersions.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, lib, ... }: {
+
+
+# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		javaVersions.enable = lib.mkOption {
+			default = true;	
+		};
+	};
+	
+	config = lib.mkIf config.javaVersions.enable {
+		# Actual content of the module goes here:
+
+		environment.systemPackages = with pkgs; [ 
+
+			terumin-jre-bin-24
+			zulu-24
+			
+		];
+
+	};		
+	
+		
+}
