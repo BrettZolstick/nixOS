37e42ef865f002ac2c72bc58eb598cacc8e35bd5
Ethan <crazyeman83@gmail.com>
2025-10-25 23:01:59 -0400

    made cpu usage actually work (fixes #29)

--- homeManager/coreDesktop/waybar.nix
+++ homeManager/coreDesktop/waybar.nix
@@ -4,7 +4,7 @@ let
 	hardwareMonitor = pkgs.writeShellScriptBin "hardware-monitor" '' 
 		#!/bin/bash
 		
-		cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf "%.0f", usage}')
+		cpu=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.0f\n", 100 - $8}')
 		ram=$(free -h | awk '/Mem:/ {print $3}')
 		gpu=$(cat /sys/class/drm/card1/device/gpu_busy_percent)
 
