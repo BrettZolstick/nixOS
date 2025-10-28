093011935cdab23931c8f0750e4f2ffe9de7e9be
Ethan <crazyeman83@gmail.com>
2025-10-27 23:59:57 -0400

    added dotnet runtime

---
+++ homeManager/coreDesktop/dotnet.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		dotnet.enable = lib.mkOption {
+			default = true;	
+		};
+	};
+	
+	config = lib.mkIf config.dotnet.enable {
+		# Actual content of the module goes here:
+
+       	home.packages = with pkgs; [ dotnet-runtime_7 ];
+		
+	};	
+}
