afb474c8b863c4f66b3c264d31fa5f39ad5187fd
Ethan <crazyeman83@gmail.com>
2025-11-05 21:00:07 -0500

    added mullvad

---
+++ nixOSConfiguration/baseSystemPackages/mullvad.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		mullvad.enable = lib.mkOption {
+			default = true;	
+		};
+	};
+	
+	config = lib.mkIf config.mullvad.enable {
+		# Actual content of the module goes here:
+
+		services.mullvad-vpn.enable = true;
+
+	};		
+
+
+
+}
