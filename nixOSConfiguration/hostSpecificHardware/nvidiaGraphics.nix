e5258539310759398d17bc7fe3da661de90190d6
Ethan <crazyeman83@gmail.com>
2025-11-13 12:23:40 -0500

    added support for nvidia graphics on laptop

---
+++ nixOSConfiguration/hostSpecificHardware/nvidiaGraphics.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, ... }: {
+
+	# This is wrapped in an option so that it can be easily toggled elsewhere.
+	options = {
+		nvidiaGraphics.enable = lib.mkOption {
+			default = false;	
+		};
+	};
+
+	config = lib.mkIf config.nvidiaGraphics.enable {
+		# Actual content of the module goes here:
+
+		hardware.graphics = {
+			enable = true;
+			enable32Bit = true;	
+		};
+
+		hardware.nvidia = {
+			modesetting.enable = true;
+			package = config.boot.kernalPackages.nvidiaPackages.stable;
+			powerManagement.enable = true;
+			powerManagement.finegrained = true; # turns off gpu when not in use
+			open = false; # use open source driver
+			nvidiaSettings = true;
+		};
+	};			
+}
