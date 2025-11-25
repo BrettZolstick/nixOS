3ddb8d25990cbc84946d1705614277ac69570421
Ethan <crazyeman83@gmail.com>
2025-11-25 03:17:19 -0500

    added rebuild alias

--- homeManager/coreDesktop/fish.nix
+++ homeManager/coreDesktop/fish.nix
@@ -26,6 +26,7 @@
 			functions = {
 				copypartyJournal = "sudo journalctl -u copyparty.service --since=@$(stat -c %Y /run/current-system) -f";
 				ethanServerSSH = "ssh ethan@192.168.68.67";
+				rbs = "sudo nixos-rebuild switch --flake ~/nixOS#$hostname --impure --show-trace";
 			};
 		};
 	};	
