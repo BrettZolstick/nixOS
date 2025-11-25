604ff8ab9c48be18d565260113218d04fc138a0e
Ethan <crazyeman83@gmail.com>
2025-11-24 19:48:19 -0500

    added starship hostname module

--- homeManager/coreDesktop/starship.nix
+++ homeManager/coreDesktop/starship.nix
@@ -16,7 +16,7 @@ let
 	timeBg		= colors.base01;
 	timeFg		= colors.base05;
 	cmdDuration	= colors.base02;
-	
+
 in
 {
 
@@ -34,11 +34,12 @@ in
 			enable = true;
 			enableFishIntegration = true;
 			settings = {
-				format = "[](${logoBg})$hostname[  ](bg:${logoBg} fg:${logoFg})[](fg:${logoBg} bg:${directoryBg})$directory[](fg:${directoryBg} bg:${gitBg})$git_branch$git_status[](fg:${gitBg} bg:${devLangBg})$nodejs$rust$golang$php[](fg:${devLangBg} bg:${timeBg})$time[](fg:${timeBg})$cmd_duration\n$character";
+				format = "[](${logoBg})[  ](bg:${logoBg} fg:${logoFg})$hostname[](fg:${logoBg} bg:${directoryBg})$directory[](fg:${directoryBg} bg:${gitBg})$git_branch$git_status[](fg:${gitBg} bg:${devLangBg})$nodejs$rust$golang$php[](fg:${devLangBg} bg:${timeBg})$time[](fg:${timeBg})$cmd_duration\n$character";
 
 				hostname = {
-					ssh_only = true;
-					format = "[ $hostname ](bg:$logoBg fg:${logoFg})";
+					ssh_only = false;
+					ssh_symbol = " ";
+					format = "[$hostname $ssh_symbol]($style)";
 					style = "bg:${logoBg} fg:${logoFg}";
 				};
 
