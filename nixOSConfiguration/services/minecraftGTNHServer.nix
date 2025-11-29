7d85108c910b8b5fc6fc7c8c518f150b6f2b5a55
Ethan <crazyeman83@gmail.com>
2025-11-29 02:45:41 -0500

    set up GTNH server

--- nixOSConfiguration/services/minecraftGTNHServer.nix
+++ nixOSConfiguration/services/minecraftGTNHServer.nix
@@ -10,18 +10,52 @@
 	config = lib.mkIf config.minecraftGTNHServer.enable {
 		# Actual content of the module goes here:
 
+		# make user and group for service
 		users.users.minecraftGTNH = {
 			isSystemUser = true;
 			home = "/srv/minecraftGTNHServer";
 			createHome = false;
+			group = "minecraftGTNH";
 		};
+		users.groups.minecraftGTNH = {};
 
-		environment.systemPackages = with pkgs; [
-			 pkgs.javaPackages.compiler.temurin-bin.jre-25
+		# grant file permissions for minecraftGTNH user
+		systemd.tmpfiles.rules = [
+			"d /srv/minecraftGTNHServer 0750 minecraft -" # creates dir if not exist already
+			"Z /srv/minecraftGTNHServer 0750 minecraft minecraft -" # recursively sets perms
 		];
 
+	
+		# install desired packages
+		environment.systemPackages = with pkgs; [
+			mcrcon # minecraft remote console
+		];
+
+
+		# open ports
 		networking.firewall.allowedTCPPorts = [ 25565 ];
 
+
+		# create systemd service
+		systemd.services.gtnh = {
+			description = "Gregtech New Horizons minecraft server";
+			wantedBy = [ "multi-user.target" ];
+			after = [ "network.target" ];
+
+			serviceConfig = {
+				WorkingDirectory = "/srv/minecraftGTNHServer";
+				User = "minecraftGTNH";
+				Group = "minecraftGTNH";
+				ExecStart = "/bin/sh /srv/minecraftGTNHServer/startserver-java9.sh";
+				Restart = "on-failure";
+				RestartSec = 10;
+				LimitNOFILE = 65535;	
+			};
+
+			path = [ pkgs.javaPackages.compiler.temurin-bin.jre-25 ];
+		};		
+		
+		
 
 	};	
 }
