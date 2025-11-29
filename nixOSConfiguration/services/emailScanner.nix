b9ecc7c30b9482ce457031a5df49dc16c60c2b0d
Ethan <crazyeman83@gmail.com>
2025-11-29 05:30:06 -0500

    made systemd service for a powershell script I use for work

--- nixOSConfiguration/services/emailScanner.nix
+++ nixOSConfiguration/services/emailScanner.nix
@@ -10,35 +10,41 @@
 	config = lib.mkIf config.emailScanner.enable {
 		# Actual content of the module goes here:
 
-			# This is an old powershell script that I wrote for my business
+		# This is an old powershell script that I wrote for my business
 
-				# make a user and group for the service
-				users = {
-					users.emailScanner = {
-						isSystemUser = true;
-						home = "/srv/copyparty/prep/NewQA/EpcListEnhancer2";
-						createHome = false;
-						group = "emailScanner"; 
-					};
-					groups.emailScanner = {};
-				};			
-			
-				systemd.services.emailScanner = {
-					description = "Scans email for new epc lists";
-					wantedBy = [ "multi-user.target" ];
-					after = [ "network-online.target" ];
-					wants = [ "network-online.target" ];
-					serviceConfig = {
-						Type = "simple";
-						User = "emailScanner";
-						WorkingDirectory = "/srv/copyparty/prep/NewQA/EpcListEnhancer2";
-						EnvironmentFile = "/etc/emailscanner-imap-creds.env";
-						ExecStart = "${pkgs.powershell}/bin/pwsh -File ./EmailScanner.ps1";
-						Restart = "on-failure";
-						RestartSec = 10;	
-					};
-				};			
-			
-		};	
-	};		
+		# make a user and group for the service
+		users = {
+			users.emailScanner = {
+				isSystemUser = true;
+				home = "/srv/copyparty/prep/NewQA/EpcListEnhancer2";
+				createHome = false;
+				group = "emailScanner"; 
+				extraGroups = [ "copyparty" ];
+			};
+			groups.emailScanner = {};
+		};			
+	
+		# Make service
+		systemd.services.emailScanner = {
+			description = "Scans email for new epc lists";
+			wantedBy = [ "multi-user.target" ];
+			after = [ "network-online.target" ];
+			wants = [ "network-online.target" ];
+			serviceConfig = {
+				Type = "simple";
+				User = "emailScanner";
+				WorkingDirectory = "/srv/copyparty/prep/NewQA/EpcListEnhancer2";
+				EnvironmentFile = "/etc/emailscanner-imap-creds.env";
+				ExecStart = "${pkgs.powershell}/bin/pwsh -File ./EmailScanner.ps1";
+				Restart = "on-failure";
+				RestartSec = 10;	
+			};
+		};			
+
+		# Add powershell to environment systempackages for debugging purposes
+		environment.systemPackages = with pkgs; [
+			powershell
+		];
+	};	
 }
+
