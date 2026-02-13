7ebd4b90e64024c2e18840b53dc6d6d0e97f83f3
Ethan <crazyeman83@gmail.com>
2026-02-13 05:08:02 -0500

    made a vanilla minecraft server

---
+++ nixOSConfiguration/services/minecraftVanillaServer.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  # This is wrapped in an option so that it can be easily toggled elsewhere.
+  options = {
+    minecraftVanillaServer.enable = lib.mkOption {
+      default = false;
+    };
+  };
+
+  config = lib.mkIf config.minecraftVanillaServer.enable {
+
+    services.minecraft-server = {
+      enable = true;
+      eula = true;
+      openFirewall = true;
+      declarative = true;
+      package = pkgs.papermcServers.papermc-1_21_11;
+      serverProperties = {
+        server-port = 25566;
+        difficulty = 3;
+        gamemode = 1;
+        max-players = 5;
+        motd = "Epic Skyblock";
+        white-list = false;
+        enable-rcon = true;
+        "rcon.password" = "block";
+      };
+      jvmOpts = "-Xms6092M -Xmx6092M -XX:+UseG1GC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10";
+    };
+        
+  };
+}
