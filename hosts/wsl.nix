600981de4adeefd64e78c3eaebdf88883dfa630c
BrettZolstick <64443654+BrettZolstick@users.noreply.github.com>
2026-05-22 09:44:25 -0400

    Add WSL NixOS configuration with imports and options

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -1,1 +1,24 @@
+{lib, ...}: {
+  imports =
+    [
+      # Imports hardware-configuration.nix from the default location
+      # this allows for the configuration to be built straight from github on any NixOS with flakes enabled.
+      /etc/nixos/hardware-configuration.nix # needs the --impure switch when running nixos-rebuild
 
+      # include a list of all .nix files recursively under this directory					  v---------------------v
+    ]
+    ++ lib.filter (n: lib.strings.hasSuffix ".nix" n) (lib.filesystem.listFilesRecursive ../nixOSConfiguration/.);
+
+  # Host specific options ################################################
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  # Optional Modules #####################################################
+
+  home-manager.users.ethan = {
+  
+  };
+}
