6acf528c83c8bcf5f61722f1913b5c72acf8fc7d
Ethan <crazyeman83@gmail.com>
2026-08-10 04:12:47 -0400

    added unzip

---
+++ homeManager/coreDesktop/unzip.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  # This is wrapped in an option so that it can be easily toggled elsewhere.
+  options = {
+    unzip.enable = lib.mkOption {
+      default = true;
+    };
+  };
+
+  config = lib.mkIf config.unzip.enable {
+    # Actual content of the module goes here:
+    home.packages = with pkgs; [unzip];
+  };
+}
