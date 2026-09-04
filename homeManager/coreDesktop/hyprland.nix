dc31dd273ce112fdc5480bacdb577d1bb987df1d
Ethan <crazyeman83@gmail.com>
2026-09-04 05:00:41 -0400

    added monitors.lua

--- homeManager/coreDesktop/hyprland.nix
+++ homeManager/coreDesktop/hyprland.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  osConfig,
   ...
 }: {
   # This is wrapped in an option so that it can be easily toggled elsewhere.
@@ -28,25 +29,31 @@
         "${config.home.homeDirectory}/nixOS/homeManager/coreDesktop/hyprland";
 
     xdg.configFile."hypr/stylix.lua".text = ''
-        return {
-          base00 = "${config.lib.stylix.colors.withHashtag.base00}",
-          base01 = "${config.lib.stylix.colors.withHashtag.base01}",
-          base02 = "${config.lib.stylix.colors.withHashtag.base02}",
-          base03 = "${config.lib.stylix.colors.withHashtag.base03}",
-          base04 = "${config.lib.stylix.colors.withHashtag.base04}",
-          base05 = "${config.lib.stylix.colors.withHashtag.base05}",
-          base06 = "${config.lib.stylix.colors.withHashtag.base06}",
-          base07 = "${config.lib.stylix.colors.withHashtag.base07}",
-          base08 = "${config.lib.stylix.colors.withHashtag.base08}",
-          base09 = "${config.lib.stylix.colors.withHashtag.base09}",
-          base0A = "${config.lib.stylix.colors.withHashtag.base0A}",
-          base0B = "${config.lib.stylix.colors.withHashtag.base0B}",
-          base0C = "${config.lib.stylix.colors.withHashtag.base0C}",
-          base0D = "${config.lib.stylix.colors.withHashtag.base0D}",
-          base0E = "${config.lib.stylix.colors.withHashtag.base0E}",
-          base0F = "${config.lib.stylix.colors.withHashtag.base0F}",
+      return {
+        base00 = "${config.lib.stylix.colors.withHashtag.base00}",
+        base01 = "${config.lib.stylix.colors.withHashtag.base01}",
+        base02 = "${config.lib.stylix.colors.withHashtag.base02}",
+        base03 = "${config.lib.stylix.colors.withHashtag.base03}",
+        base04 = "${config.lib.stylix.colors.withHashtag.base04}",
+        base05 = "${config.lib.stylix.colors.withHashtag.base05}",
+        base06 = "${config.lib.stylix.colors.withHashtag.base06}",
+        base07 = "${config.lib.stylix.colors.withHashtag.base07}",
+        base08 = "${config.lib.stylix.colors.withHashtag.base08}",
+        base09 = "${config.lib.stylix.colors.withHashtag.base09}",
+        base0A = "${config.lib.stylix.colors.withHashtag.base0A}",
+        base0B = "${config.lib.stylix.colors.withHashtag.base0B}",
+        base0C = "${config.lib.stylix.colors.withHashtag.base0C}",
+        base0D = "${config.lib.stylix.colors.withHashtag.base0D}",
+        base0E = "${config.lib.stylix.colors.withHashtag.base0E}",
+        base0F = "${config.lib.stylix.colors.withHashtag.base0F}",
+      }
+    '';
+
+      xdg.configFile."hypr/host.lua".text = ''
+        return{
+          currentHost = "${osConfig.networking.hostName}"
         }
       '';
-    
+      
   };
 }
