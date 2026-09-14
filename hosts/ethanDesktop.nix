15004e831cb0055647dd5b516db7faf3ed27285b
Ethan <crazyeman83@gmail.com>
2026-09-14 02:03:04 -0400

    disabled chromium

--- hosts/ethanDesktop.nix
+++ hosts/ethanDesktop.nix
@@ -78,7 +78,7 @@
     mangojuice.enable = true;
     # xmage.enable = true;
     easyEffects.enable = true;
-    chromium.enable = true;
+    # chromium.enable = true;
     # qemu.enable = true;
     python.enable = true;
     # kdenlive.enable = true;
